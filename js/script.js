dd2ef3f5fae06b048d5db49742a6b5a012962f59
Tonix89 <Antonio.Arabejo@stud.noroff.no>
2022-06-19 07:34:48 +0200

    adding answers

---
+++ js/script.js
@@ -0,0 +1,92 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// Question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meow");
+    }
+};
+
+cat.complain();
+
+// Question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// QUestion 4
+
+heading.classList.add("subheading");
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+
+// Question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = `<p>New paragraph</p>`;
+
+resultsContainer.style.background = "yellow";
+
+// Question 7
+
+function catList(list){
+    for ( let i = 0; i < list.length; i++){
+        let catName = list[i];
+        console.log(catName.name);
+    }
+}
+
+catList(cats);
+
+// Question 8
+
+let catContainer = document.querySelector(".cat-container");
+
+function createCats(cats){
+
+    let catListResult = "";
+
+    for(let i = 0; i < cats.length; i++){
+        let catList = cats[i];
+        let catName = catList.name;
+        let catAge = catList.age;
+        if(catAge === undefined){
+            catAge = "Age unknown";
+        }
+
+        catListResult = catListResult + `<h5>Name : ${catName}</h5>
+            <p>Age : ${catAge}</p>
+        `;
+    }
+
+    return catListResult;
+}
+
+const finalCatList = createCats(cats);
+
+catContainer.innerHTML = finalCatList;
